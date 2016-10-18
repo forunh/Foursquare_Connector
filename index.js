9a2e219799214c7e7c75f92813020c6b55f2d644
Pittayarat <pittayarat.sae@gmail.com>
2016-10-19 04:49:31 +0700

    add swagger

--- index.js
+++ index.js
@@ -3,8 +3,8 @@ import express from 'express'
 import bodyParser from 'body-parser'
 import foursquareRouter from './controller/FoursquareController'
 import cors from 'cors'
-// import swagger from 'swagger-node-express'
-// import { applicationUrl, swaggerPath } from './swagger'
+import swagger from 'swagger-node-express'
+import { applicationUrl, swaggerPath } from './swagger'
 
 
 
@@ -27,6 +27,23 @@ app.route('/').get((req, res) => {
 })
 
 app.use('/foursquare', foursquareRouter)
+
+
+//Swagger
+app.use('/swagger', swaggerPath)
+swagger.setAppHandler(swaggerPath)
+app.use(express.static(__dirname + '/dist'))
+swagger.setApiInfo({
+  title: "Facebook Connector API",
+  description: "API connect to the Facebook",
+  termsOfServiceUrl: "",
+  contact: "yourname@something.com",
+  license: "",
+  licenseUrl: ""
+})
+swagger.configureSwaggerPaths('', 'api-docs', '')
+swagger.configure(applicationUrl, '1.0.0')
+
 
 //service start
 app.listen(port, () => {
