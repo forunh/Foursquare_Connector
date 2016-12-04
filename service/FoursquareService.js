4f5b90b18f475f6522d8cea2e61161a35e293ace
Pittayarat <pittayarat.sae@gmail.com>
2016-12-04 23:21:44 +0700

    edit cron

--- service/FoursquareService.js
+++ service/FoursquareService.js
@@ -2,16 +2,23 @@ import request from 'request'
 import cron from 'cron'
 import { db } from '../db'
 
+// let params ={
+//             // ll:'40.7,-74',
+//             client_id : 'VYPDJ2Z5QROWDVGISDDRSS2MWQTARTSUU0LKLK3BONI03W35',
+//             client_secret : 'TSJ5KW4EQYLGA5MKDKKAXFFJQJ5KGCUUCN3H3DWUJBTYJ4CQ',
+//             v : today()
+            
+// }
 let params ={
             // ll:'40.7,-74',
-            client_id : 'VYPDJ2Z5QROWDVGISDDRSS2MWQTARTSUU0LKLK3BONI03W35',
-            client_secret : 'TSJ5KW4EQYLGA5MKDKKAXFFJQJ5KGCUUCN3H3DWUJBTYJ4CQ',
+            client_id : '2ANDJSV5P2FT3GNVOQNSDQQSPJ5XRNO5XQR2COKWNG0I1K5C',
+            client_secret : 'NXTDWJVI4RNEKJIBAUIQBKCHL1IWYIJST1YOKOJOYFWNY4KT',
             v : today()
             
 }
 const cronJob = cron.CronJob
-const venueIDs = ['4c034d0cf56c2d7fa6c71c66']
-//const venueIDs = ['4c034d0cf56c2d7fa6c71c66','4af833a6f964a5205a0b22e3','4b0587fdf964a52034ab22e3']
+// const venueIDs = ['4c034d0cf56c2d7fa6c71c66']
+const venueIDs = ['4c034d0cf56c2d7fa6c71c66','4af833a6f964a5205a0b22e3','4b0587fdf964a52034ab22e3']
 //kmitl airport siamparagon
 export function searchVenues(geolocation){
     params.ll = geolocation
@@ -121,12 +128,15 @@ function saveFQvenue (venueID){
   db.FQ_VENUE.findOne({id: venueID}, (err, document) => {
     if(!document) {
       VenueDetail(venueID).then(page => {
-        const res = JSON.parse(page)          
-        db.FQ_VENUE.insert(res.response.venue, err => {
-          if(err) {
-            console.log(err)
-          }
-        })
+        const res = JSON.parse(page) 
+        if(res.meta.code == 200)    
+        {         
+            db.FQ_VENUE.insert(res.response.venue, err => {
+            if(err) {
+                console.log(err)
+            }
+            })
+        }
       })
     }
   })
@@ -135,82 +145,91 @@ function saveFQvenue (venueID){
 function saveFQtip (venueID){
     venueTips(venueID).then(res => {
         const x = JSON.parse(res)
-        x.response.tips.items.forEach(tip => {
-            db.FQ_TIP.findOne({id : tip.id},(err,document) => {
-                if(!document){
-                    tip.venueId = venueID
-                    db.FQ_TIP.insert(tip, err => {
-                        if(err){
-                            console.log(err)
-                        }
-                    })
-                }
+        if(x.meta.code == 200)    
+        {
+            x.response.tips.items.forEach(tip => {
+                db.FQ_TIP.findOne({id : tip.id},(err,document) => {
+                    if(!document){
+                        tip.venueId = venueID
+                        db.FQ_TIP.insert(tip, err => {
+                            if(err){
+                                console.log(err)
+                            }
+                        })
+                    }
+                })
             })
-        })
+        }
     })
 }
 
 function saveFQphoto (venueID){
     VenuePhoto(venueID).then(res => {
         const x = JSON.parse(res)        
-        x.response.photos.items.forEach(photo => {
-            db.FQ_PHOTO.findOne({id : photo.id},(err,document) => {
-                if(!document){
-                    photo.venueId = venueID
-                    db.FQ_PHOTO.insert(photo, err => {
-                        if(err){
-                            console.log(err)
-                        }
-                    })
-                }
+        if(x.meta.code == 200)    
+        {
+            x.response.photos.items.forEach(photo => {
+                db.FQ_PHOTO.findOne({id : photo.id},(err,document) => {
+                    if(!document){
+                        photo.venueId = venueID
+                        db.FQ_PHOTO.insert(photo, err => {
+                            if(err){
+                                console.log(err)
+                            }
+                        })
+                    }
+                })
             })
-        })
+        }
     })
 }
 
 function saveFQcheckin (venueID){
     VenueHereNow(venueID).then(res => {
-        const x = JSON.parse(res)        
-        const checkin = x.response.hereNow
-        // db.FQ_PHOTO.findOne({id : photo.id},(err,document) => {
-            // if(!document){  
-                checkin.venueId = venueID
-                checkin.datetime = new Date().toString()
-                db.FQ_PHOTO.insert(checkin, err => {
-                    if(err){
-                        console.log(err)
-                    }
-                })
-            // }
-        // })
+        const x = JSON.parse(res)   
+        if(x.meta.code == 200)    
+        {
+            const checkin = x.response.hereNow
+            checkin.venueId = venueID
+            checkin.datetime = new Date().toString()
+            db.FQ_CHECKIN.insert(checkin, err => {
+                if(err){
+                    console.log(err)
+                }
+            })
+        }   
     })
 }
 
 function saveFQpopularHour (venueID){
     VenueHours(venueID).then(res => {
-        const x = JSON.parse(res)        
-        const hours = x.response.popular
-        hours.venueId = venueID
-        hours.lastUpdated = new Date().toString()
-        db.FQ_POPULARHOUR.update({venueId : venueID},hours,(err,document) => {
-            // if(!document){  
-                // db.FQ_POPULARHOUR.insert(checkin, err => {
-                    if(err){
-                        console.log(err)
-                    }
-                // })
-            // }
-        })
+        const x = JSON.parse(res)    
+        if(x.meta.code == 200)    
+        {    
+            const hours = x.response.popular
+            hours.venueId = venueID
+            hours.lastUpdated = new Date().toString()
+            db.FQ_POPULARHOUR.update({venueId : venueID},hours,(err,document) => {
+                // if(!document){  
+                    // db.FQ_POPULARHOUR.insert(checkin, err => {
+                        if(err){
+                            console.log(err)
+                        }
+                    // })
+                // }
+            })
+        }
     })
 }
 
-const cronSave30 = new cronJob('*/30 * * * * *', () => {
+const cronSave1min = new cronJob('00 * * * * *', () => {
   venueIDs.forEach(venueID => {
     saveFQtip(venueID)
     saveFQphoto(venueID)
     saveFQcheckin(venueID)
-    
   })
+    console.log(new Date().toString())
+  
 },
 () => {
   console.log('cronSaveFBcomment')
@@ -218,11 +237,14 @@ const cronSave30 = new cronJob('*/30 * * * * *', () => {
 true
 )
 
-const cronSave60 = new cronJob('*/30 * * * * *', () => {
+const cronSave1hr = new cronJob('00 21 23 * * *', () => {
   venueIDs.forEach(venueID => {
     saveFQpopularHour(venueID)
     saveFQvenue(venueID)
+    
   })
+    console.log(new Date().toString())
+  
 },
 () => {
   console.log('cronSaveFBcomment')
